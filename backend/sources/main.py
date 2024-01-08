0df246121e3bb6998b847c400fc76d29e35b3a69
Taehoon <lth9029@gmail.com>
2024-01-08 11:54:33 +0900

    complete test for connection between flask and vue

--- backend/sources/main.py
+++ backend/sources/main.py
@@ -1,42 +1,14 @@
-from flask import Flask, render_template
-from flask import url_for, session, request, redirect
-from datetime import datetime
-from flask_cors import CORS
+from flask import Flask, render_template, make_response
+import os
+app = Flask(__name__
+            , template_folder=os.getcwd()+'../../frontend/dist'
+            , static_folder=os.getcwd()+'../../frontend/dist/static'
+            , static_url_path='/static')
 
-app = Flask(__name__)
-# db = SQLAlchemy(app)
-CORS(app)
+@app.route("/", methods=["GET"])
+def page_index():
+    resp = make_response(render_template("index.html"))
+    return resp
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/event", methods=['POST'])
-def create_event():
-    description = request.json['description']
-
-@app.route('/users')
-def users():
-	# users 데이터를 Json 형식으로 반환한다
-    print("test!!")
-    return {"members": [{ "id" : 1, "name" : "yerin" },
-    					{ "id" : 2, "name" : "dalkong" }]}
-
-@app.route('/whynot', methods = ['GET','POST'])
-def postTest():
-    if request.method == 'GET':
-        return {"members": [{ "id" : 1, "name" : "yerin" },
-    					{ "id" : 2, "name" : "dalkong" }]}
-    elif request.method == 'POST':
-        print("post test")
-        username = request.json("username")
-        password = request.json("password")
-        print(username)
-        print(password)
-        return {"members": [{ "id" : 1, "name" : "yerin" },
-    					{ "id" : 2, "name" : "dalkong" }]}
-
-	
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=80) 
+if __name__ == "__main__":
+    app.run()
